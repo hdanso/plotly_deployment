a930ea81763686a50ae777f87a5d8685ee4998c4
Henrietta Danso <danso.henrietta@gmail.com>
2022-11-25 18:56:46 -0500

    updating file

--- charts.js
+++ charts.js
@@ -101,7 +101,7 @@ function buildCharts(sample) {
   // BUBBLE CHART
 
     // 1. Create the trace for the bubble chart.
-    var trace2 = {
+    var bubbleData = {
       x: otuID,
       y: otuValue,
       text: otuLabel,
@@ -111,15 +111,18 @@ function buildCharts(sample) {
         size: [otuValue]
       }
     };
-    var bubbleData = [trace2]
+
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       xaxis: "OTU ID"
-    });
-  }
+    }
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+    Plotly.newPlot("bubble", [bubbleData], bubbleLayout); 
   });
 }
+
+ 
+
+
